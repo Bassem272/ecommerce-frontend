9b9241298999f133261a77552c722e7ba1467759
bassem <bassem.gehad95@yahoo.com>
2025-01-04 14:20:20 +0200

    uri

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:8080/', // Replace with your GraphQL endpoint
+  uri: 'https://f22c-197-32-64-10.ngrok-free.app', // Replace with your GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
